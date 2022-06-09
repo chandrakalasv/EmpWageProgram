2972c00f32297d6659dd5909802c60a6dfbf8053
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-09 15:24:28 +0530

    added code to calculate emp monthly wage

--- src/com/bridgelab/emp/wageComputation.java
+++ src/com/bridgelab/emp/wageComputation.java
@@ -2,11 +2,14 @@ package com.bridgelab.emp;
 
 public class wageComputation {
     public static int wagePerHour = 20;
-    public int fullDayHour = 8;
+    public int fullTimeHour = 8;
     public int partTimeHour = 4;
+    public int workingDay = 0;
+    int totalFullTimeEmpWage = 0;
+    int totalPartTimeEmpWage = 0;
 
     public int empFullDayWage() {
-        return wagePerHour * fullDayHour;
+        return wagePerHour * fullTimeHour;
     }
 
     public int empPartTimeWage() {
@@ -14,22 +17,29 @@ public class wageComputation {
     }
 
     public void isEmpPresent() {
-        int checkAttendence = (int) (Math.random() * 10) % 3;
-        switch (checkAttendence) {
-            case 1: {
-                int empFullDayWage = empFullDayWage();
-                System.out.println("emp is present and full time emp daily wage is" + " " + empFullDayWage);
-                break;
+        while(workingDay <= 20) {
+            int checkAttendence = (int) (Math.random() * 10) % 3;
+            switch (checkAttendence) {
+                case 1: {
+                    int empFullDayWage = empFullDayWage();
+                    totalFullTimeEmpWage = totalFullTimeEmpWage + empFullDayWage;
+                    System.out.println("emp is present and full time emp daily wage is" + " " + empFullDayWage);
+                    break;
+                }
+                case 2: {
+                    int empPartTimeWage = empPartTimeWage();
+                    totalPartTimeEmpWage = totalPartTimeEmpWage + empPartTimeWage;
+                    System.out.println("emp is present and part time emp daily wage is" + " " + empPartTimeWage);
+                    break;
+                }
+                case 0:
+                    System.out.println("emp is absent");
+                    break;
             }
-            case 2: {
-                int empPartTimeWage = empPartTimeWage();
-                System.out.println("emp is present and part time emp daily wage is" + " " + empPartTimeWage);
-                break;
-            }
-            case 0:
-                System.out.println("emp is absent");
-                break;
+            workingDay++;
         }
+        System.out.println("total working day" + " " + workingDay);
+        System.out.println("total monthly wage is" + " " + (totalFullTimeEmpWage+totalPartTimeEmpWage));
     }
 }
 
