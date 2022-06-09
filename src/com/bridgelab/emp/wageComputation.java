79dfbbfb5118bf9cc6292f90e1ce7e9b2f3fe61e
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-09 14:29:57 +0530

    added code to calcualte full time emp daily wage

--- src/com/bridgelab/emp/wageComputation.java
+++ src/com/bridgelab/emp/wageComputation.java
@@ -1,11 +1,19 @@
 package com.bridgelab.emp;
 
 public class wageComputation {
+    public static int wagePerHour = 20;
+    public int fullDayHour = 8;
+
+    public int empFullDayWage() {
+        return wagePerHour * fullDayHour;
+    }
+
     public void isEmpPresent() {
         int checkAttendence = (int) (Math.random() * 10) % 2;
-        if (checkAttendence == 1)
-            System.out.println("emp is present");
-        else if (checkAttendence == 0) {
+        if (checkAttendence == 1) {
+            int empFullDayWage = empFullDayWage();
+            System.out.println("emp is present and full time emp daily wage is" + " " + empFullDayWage);
+        } else if (checkAttendence == 0) {
             System.out.println("emp is absent");
         }
     }
