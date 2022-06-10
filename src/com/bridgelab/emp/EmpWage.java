596f025a53da78199f6ff2804d6873bdee6fcdcc
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-10 08:50:09 +0530

    added code to manage multiple company emp wage in array

--- src/com/bridgelab/emp/EmpWage.java
+++ src/com/bridgelab/emp/EmpWage.java
@@ -7,19 +7,38 @@ public class EmpWage {
 
     public static void main(String[] args) {
         System.out.println("welcome to employee wage program");
+        WageComputation[] wage = new WageComputation[4];
         int select;
         do {
             System.out.println("choose company name" + "\n 1. TCS" + "\n 2.HCL" + "\n 3. INFOSYS" + "\n 4. MIND-TREE");
             int companyName = choice.nextInt();
             switch (companyName) {
-                case 1 -> new WageComputation("TCS", 20, 100, 20);
-                case 2 -> new WageComputation("HCL",10, 200, 30);
-                case 3 -> new WageComputation("INFOSYS", 8, 50, 40);
-                case 4 -> new WageComputation("MIND-TREE", 16, 70, 50);
-                default -> System.out.println("please choose proper company");
+                case 1: {
+                    wage[0] = new WageComputation("TCS", 20, 100, 20);
+                    wage[0].display();
+                    break;
+                }
+                case 2: {
+                    wage[1] = new WageComputation("HCL", 10, 200, 30);
+                    wage[1].display();
+                    break;
+                }
+                case 3: {
+                    wage[2] = new WageComputation("INFOSYS", 8, 50, 40);
+                    wage[2].display();
+                    break;
+                }
+                case 4: {
+                    wage[3] = new WageComputation("MIND-TREE", 16, 70, 50);
+                    wage[3].display();
+                    break;
+                }
+                default:
+                    System.out.println("please choose proper company");
             }
             System.out.println("are you wish to continue:" + "\n 1. continue" + "\n 2. exit");
             select = choice.nextInt();
         } while (select == 1);
+
     }
 }
--- src/com/bridgelab/emp/WageComputation.java
+++ src/com/bridgelab/emp/WageComputation.java
@@ -7,29 +7,35 @@ public class WageComputation {
    public int workingHour = 0;
    public int fullTimeWorkingHour = 0;
    public int partTimeWorkingHour = 0;
+   public String companyName;
 
    public WageComputation(String companyName, int wagePerHour, int workingHourPerMonth, int workingDayPerMonth) {
-        while ((workingHour <= workingHourPerMonth) && (totalWorkingDay < workingDayPerMonth)) {
-            int checkAttendence = (int) (Math.random() * 10) % 3;
-            switch (checkAttendence) {
-                case 1: {
-                    int empFullDayWage = wagePerHour * 8;
-                    totalFullTimeEmpWage = totalFullTimeEmpWage + empFullDayWage;
-                    fullTimeWorkingHour = fullTimeWorkingHour + 8;
-                    break;
-                }
-                case 2: {
-                    int empPartTimeWage = wagePerHour * 4;
-                    totalPartTimeEmpWage = totalPartTimeEmpWage + empPartTimeWage;
-                    partTimeWorkingHour = partTimeWorkingHour + 4;
-                    break;
-                }
-            }
-            totalWorkingDay++;
-        }
+       this.companyName = companyName;
+       while ((workingHour <= workingHourPerMonth) && (totalWorkingDay < workingDayPerMonth)) {
+           int checkAttendence = (int) (Math.random() * 10) % 3;
+           switch (checkAttendence) {
+               case 1: {
+                   int empFullDayWage = wagePerHour * 8;
+                   totalFullTimeEmpWage = totalFullTimeEmpWage + empFullDayWage;
+                   fullTimeWorkingHour = fullTimeWorkingHour + 8;
+                   break;
+               }
+               case 2: {
+                   int empPartTimeWage = wagePerHour * 4;
+                   totalPartTimeEmpWage = totalPartTimeEmpWage + empPartTimeWage;
+                   partTimeWorkingHour = partTimeWorkingHour + 4;
+                   break;
+               }
+           }
+           totalWorkingDay++;
+       }
+   }
+    public void display() {
+        System.out.println("company name is  " + companyName);
         System.out.println("total working day" + " " + totalWorkingDay);
         System.out.println("total working hours is" + " " + (fullTimeWorkingHour + partTimeWorkingHour));
         System.out.println("total monthly wage is" + " " + (totalFullTimeEmpWage + totalPartTimeEmpWage));
     }
+
 }
 
